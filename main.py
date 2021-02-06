27f0ed2e1434a326c16dd080a7a598c5274f1796
GaliaMalasa <galiyam1093@gmail.com>
2021-02-06 14:45:26 +0200

    BarChart addition

--- main.py
+++ main.py
@@ -1,16 +1,22 @@
-# This is a sample Python script.
+import openpyxl as xl
+from openpyxl.chart import BarChart, Reference
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+wb = xl.load_workbook('transactions.xlsx')
+sheet = wb['Sheet1']
+for row in range(2, sheet.max_row + 1):
+    cell = sheet.cell(row, 3)
+    corrected_price = cell.value * 0.9
+    corrected_price_cell = sheet.cell(row, 4)
+    corrected_price_cell.value = corrected_price
 
+values = Reference(sheet,
+                   min_row=2,
+                   max_row=sheet.max_row,
+                   min_col=4,
+                   max_col=4)
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+chart = BarChart()
+chart.add_data(values)
+sheet.add_chart(chart, 'e2')
 
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+wb.save('transactions2.xlsx')
